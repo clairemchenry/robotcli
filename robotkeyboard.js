e16cd75d5b04175f6e1e1719ad98e594d281d09b
clairemchenry <clairemchenry@seattleacademy.org>
2015-06-08 21:03:34 +0000

    init commit

--- robotkeyboard.js
+++ robotkeyboard.js
@@ -7,7 +7,7 @@ var irobot = require('irobot');
 
 var robot = new irobot.Robot('/dev/ttyO0');
 robot.on('ready', function () {
-  console.log('READY');
+  console.log('READY1');
 });
 // make `process.stdin` begin emitting "keypress" events
 
@@ -41,6 +41,68 @@ function fire(){
 	setTimeout(function(){ThunderConnector.command('fire');},0);
 }
 
+  // mysong =  
+  //  [ [ 392, 300 ],
+  //    [ 164.8, 600 ],
+  //    [ 130.8, 600 ],
+  //    [ 164.8, 450 ],
+  //    [ 392, 200 ],
+  //    [ 261.6, 600 ],
+  //    [ 329.6, 450 ],
+  //    [ 293.7, 200 ],
+  //    [ 261.6, 800 ],
+  //    [ 164.8, 400 ],
+  //    [ 92.5, 600 ],
+  //    [ 392, 600 ],
+  //    [ 392, 600 ] 
+  //    [ 392, 300 ],
+  //    [ 329.6, 600 ],
+  //    [ 293.7, 600 ],
+  //    [ 261.6, 450 ],
+  //    [ 246.9, 200 ],
+  //    [ 440, 600 ],
+  //    [ 246.9, 450 ],
+  //    [ 261.6, 200 ],
+  //    [ 261.6, 800 ],
+  //    [ 392, 400 ],
+  //    [ 164.8, 600 ],
+  //    [ 130.8, 600 ] ];
+ mysong =  
+   [ 
+   [ 392, 300 ],
+     [ 164.8, 300 ], 
+     [ 130.8, 600 ],
+   [ 164.8, 600 ],
+  [ 392, 600 ],
+   [ 261.6, 700 ],
+    [ 329.6, 300 ],
+    [ 293.7, 300 ],
+     [ 261.6, 600 ],
+     [ 164.8, 600 ],
+     [ 185.0, 600 ],
+     [ 196, 600 ],
+      [ 196, 300],
+     [ 196, 300],
+     [329.6, 700],
+     [293.7, 400],
+     [261.6, 500],
+     [246.9, 800],
+     [220.0, 400],
+     [246.9, 400],
+     [261.6, 500],
+     [261.6, 600],
+     [196.0, 600],
+     [164.8, 600],
+     [130.8, 600]
+      ];
+     
+        
+// robot.on('bump', function (e) {
+//     console.log('BUMP', e);
+//     console.log(mysong);
+//     robot.sing(mysong);
+// });
+
 keypress(process.stdin);
 
 // listen for the "keypress" event
@@ -49,6 +111,7 @@ process.stdin.on('keypress', function (ch, key) {
 
   if (key.name == 'w'){
   	console.log("moved up");
+    robot.sing(mysong);
   	//up(10);
     data = {left: 20, right: 20};
     robot.drive(data);
